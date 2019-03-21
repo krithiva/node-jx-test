9bc878e9abc4ebca13b966207a29279e852b133c
krithiva <krithiva.gopal@gmail.com>
2019-03-21 12:58:14 +0530

    added sonar ip

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://35.188.108.66:9000',
+       serverUrl: 'http://35.224.62.117:9000',
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : 'packages/core/src/**' // Entry point of your code
